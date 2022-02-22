b9689900d5734883f957df808ae946b437987bc4
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2022-02-22 14:32:20 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,31 +1,26 @@
 ﻿#include <iostream>;
+#include <cmath>;
 using namespace std;
-double fd(double a, double  b, double h);
-double fd(double a, double  b, double h)
+void ff(double *a, double *b);
+void ff(double *a, double *b)
 {
-	double s = (a + b) / 2 * h;
-	return s;
+	double c = *b - *a;
+	c = fabs(c);
+	cout << "dlina =" << c;
+	cout << endl;
+	return;
 }
-int  main()
+void main()
 {
-	double a1, b1, a2, b2, h1, h2, s1, s2;
-	cout << " vvedite a1 =";
+	double a1, b1, a2, b2;
+	cout << "a1= ";
 	cin >> a1;
-	cout << " vvedite b1 =";
+	cout << "b1= ";
 	cin >> b1;
-	cout << " vvedite h1 =";
-	cin >> h1;
-	cout << " vvedite a2 =";
+	ff(&a1, &b1);
+	cout << "a2= ";
 	cin >> a2;
-	cout << " vvedite b2 =";
+	cout << "b2= ";
 	cin >> b2;
-	cout << " vvedite h2 =";
-	cin >> h2;
-	s1 = fd(a1, b1, h1);
-	s2 = fd(a2, b2, h2);
-	cout << " s1 =" << s1;
-	cout << endl;
-	cout << " s2 =" << s2;
-	cout << endl;
-	return 0;
+	ff(&a2, &b2);
 }
