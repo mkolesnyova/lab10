6a9b40382c40a1f1b75c594a501925f23607b668
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2022-02-25 12:24:13 +0300

    Add files via upload

---
+++ lab11.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+void ff(double &a, double &b);
+void ff(double &a, double &b)
+{
+	double c = sqrt(a * a + b * b);
+	double s = 0.5 * a * b;
+	double p = a + b + c;
+	cout << " p= " << p << "; s= " << s;
+	cout << endl;
+}
+int main()
+{
+	double a1, a2, b1, b2;
+	cout << "vvedite a1 b1: ";
+	cin >> a1 >> b1;
+	ff(a1, b1);
+	cout << endl;
+	cout << "vvedite a2 b2: ";
+	cin >> a2 >> b2;
+	ff(a2, b2);
+	cout << endl;
+}
